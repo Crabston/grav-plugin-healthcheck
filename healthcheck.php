548ac3f4a4ab4693a6f5fa6191c12044cc380506
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-04-16 17:08:08 +0200

    cleanup

--- healthcheck.php
+++ healthcheck.php
@@ -3,58 +3,50 @@ namespace Grav\Plugin;
 
 use Grav\Common\Plugin;
 
-
 /**
  * Class HealthcheckPlugin
  * @package Grav\Plugin
  */
-class HealthcheckPlugin extends Plugin
-{
+class HealthcheckPlugin extends Plugin {
 
 	/**
-	 * @var
+	 * get the subscribed events
+	 * @return array
 	 */
-	protected $uri;
-
-    /**
-     * @return array
-     *
-     */
-    public static function getSubscribedEvents(): array
-    {
-        return [
-			 'onPluginsInitialized' => ['onPluginsInitialized', 0],
-	         'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
-	         'onOutputGenerated' => ['onOutputGenerated', 0],
-        ];
-    }
-
-    /**
-     * Initialize the plugin
-     */
-    public function onPluginsInitialized(): void
-    {
-        // Don't proceed if we are in the admin plugin
-        if ($this->isAdmin()) {
-            return;
-        }
-
-	    $uri = $this->grav['uri'];
-	    $config = $this->config();
-
-	    $route = $config['route'] ?? null;
-	    if ($route && $route == $uri->path()) {
-		    $this->enable([
-			    'onPageInitialized' => ['onPageInitialized', 0]
-		    ]);
-	    }
-    }
+	public static function getSubscribedEvents(): array {
+		return [
+			'onPluginsInitialized' => ['onPluginsInitialized', 0],
+			'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+			'onOutputGenerated' => ['onOutputGenerated', 0],
+		];
+	}
 
 	/**
-	 * Send user to a random page
+	 * Initialize the plugin
 	 */
-	public function onPageInitialized(): void
-	{
+	public function onPluginsInitialized(): void {
+		// Don't proceed if we are in the admin plugin
+		if ($this->isAdmin()) {
+			return;
+		}
+
+		// Get the current URI
+		$uri = $this->grav['uri'];
+		$config = $this->config();
+		$route = $config['route'] ?? null;
+
+		// Check if the route is set and matches the current path
+		if ($route && $route == $uri->path()) {
+			$this->enable([
+				'onPageInitialized' => ['onPageInitialized', 0]
+			]);
+		}
+	}
+
+	/**
+	 * Handle the healthcheck request
+	 */
+	public function onPageInitialized(): void {
 		$payload = [
 			'status' => 200,
 			'message' => 'OK'
@@ -62,20 +54,19 @@ class HealthcheckPlugin extends Plugin
 
 		$json = json_encode($payload);
 		echo $json;
-
 	}
 
-	public function onOutputGenerated()
-	{
-
+	/**
+	 * Set the response code to 200
+	 */
+	public function onOutputGenerated() {
 		$this->grav['page']->modifyHeader('http_response_code', 200);
 	}
 
 	/**
-	 * Add current directory to twig lookup paths.
+	 * Add the plugin templates path
 	 */
-	public function onTwigTemplatePaths()
-	{
+	public function onTwigTemplatePaths() {
 		array_unshift($this->grav['twig']->twig_paths, __DIR__ . '/templates');
 	}
 }
